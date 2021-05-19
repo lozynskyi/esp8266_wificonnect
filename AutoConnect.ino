49f2bf9f7da8216cb64183c7e8b71c58036db717
Pavlo <slayter007@gmail.com>
2021-05-19 23:24:43 +0300

    Update AutoConnect.ino

--- AutoConnect.ino
+++ AutoConnect.ino
@@ -1,4 +1,10 @@
 #include <WiFiManager.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WebServer.h>
+#include <EEPROM.h>
+
 WiFiManager wm;
 
 void setup() {
@@ -23,4 +29,38 @@ void setup() {
 
 void loop() {
     wm.process();
+}
+
+//-------- Fuctions used for WiFi credentials saving and connecting to it which you do not need to change 
+bool testWifi(void)
+{
+  int c = 0;
+  Serial.println("Waiting for Wifi to connect");
+  while ( c < 20 ) {
+    if (WiFi.status() == WL_CONNECTED)
+    {
+      return true;
+    }
+    delay(500);
+    Serial.print("*");
+    c++;
+  }
+  Serial.println("");
+  Serial.println("Connect timed out, opening AP");
+  return false;
+}
+ 
+void launchWeb()
+{
+  Serial.println("");
+  if (WiFi.status() == WL_CONNECTED)
+    Serial.println("WiFi connected");
+  Serial.print("Local IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("SoftAP IP: ");
+  Serial.println(WiFi.softAPIP());
+  createWebServer();
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
 }
