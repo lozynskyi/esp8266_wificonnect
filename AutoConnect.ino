a638e5babf3278b2edb587303ce5e3c992b55fc3
Pavlo <slayter007@gmail.com>
2021-05-22 21:17:55 +0300

    Update AutoConnect.ino

--- AutoConnect.ino
+++ AutoConnect.ino
@@ -1,36 +1,105 @@
-#include <WiFiManager.h>
-
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WebServer.h>
 #include <EEPROM.h>
-
-WiFiManager wm;
-
-void setup() {
-    WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP    
-    // put your setup code here, to run once:
-    Serial.begin(115200);
-    
-    //reset settings - wipe credentials for testing
-    //wm.resetSettings();
-
-    wm.setConfigPortalBlocking(false);
-
-    //automatically connect using saved credentials if they exist
-    //If connection fails it starts an access point with the specified name
-    if(wm.autoConnect("AutoConnectAP")){
-        Serial.println("connected...yeey :)");
-    }
-    else {
-        Serial.println("Configportal running");
-    }
+ 
+//Variables
+int i = 0;
+int statusCode;
+const char* ssid = "text";
+const char* passphrase = "text";
+String st;
+String content;
+ 
+ 
+//Function Decalration
+bool testWifi(void);
+void launchWeb(void);
+void setupAP(void);
+ 
+//Establishing Local server at port 80 whenever required
+ESP8266WebServer server(80);
+ 
+void setup()
+{
+ 
+  Serial.begin(115200); //Initialising if(DEBUG)Serial Monitor
+  Serial.println();
+  Serial.println("Disconnecting previously connected WiFi");
+  WiFi.disconnect();
+  EEPROM.begin(512); //Initialasing EEPROM
+  delay(10);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.println();
+  Serial.println();
+  Serial.println("Startup");
+ 
+  //---------------------------------------- Read EEPROM for SSID and pass
+  Serial.println("Reading EEPROM ssid");
+ 
+  String esid;
+  for (int i = 0; i < 32; ++i)
+  {
+    esid += char(EEPROM.read(i));
+  }
+  Serial.println();
+  Serial.print("SSID: ");
+  Serial.println(esid);
+  Serial.println("Reading EEPROM pass");
+ 
+  String epass = "";
+  for (int i = 32; i < 96; ++i)
+  {
+    epass += char(EEPROM.read(i));
+  }
+  Serial.print("PASS: ");
+  Serial.println(epass);
+ 
+ 
+  WiFi.begin(esid.c_str(), epass.c_str());
+  if (testWifi())
+  {
+    Serial.println("Succesfully Connected!!!");
+    return;
+  }
+  else
+  {
+    Serial.println("Turning the HotSpot On");
+    launchWeb();
+    setupAP();// Setup HotSpot
+  }
+ 
+  Serial.println();
+  Serial.println("Waiting.");
+  
+  while ((WiFi.status() != WL_CONNECTED))
+  {
+    Serial.print(".");
+    delay(100);
+    server.handleClient();
+  }
+ 
 }
-
 void loop() {
-    wm.process();
+  if ((WiFi.status() == WL_CONNECTED))
+  {
+ 
+    for (int i = 0; i < 10; i++)
+    {
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(1000);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(1000);
+    }
+ 
+  }
+  else
+  {
+  }
+ 
 }
-
+ 
+ 
 //-------- Fuctions used for WiFi credentials saving and connecting to it which you do not need to change 
 bool testWifi(void)
 {
@@ -63,4 +132,52 @@ void launchWeb()
   // Start the server
   server.begin();
   Serial.println("Server started");
+}
+ 
+void setupAP(void)
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n == 0)
+    Serial.println("no networks found");
+  else
+  {
+    Serial.print(n);
+    Serial.println(" networks found");
+    for (int i = 0; i < n; ++i)
+    {
+      // Print SSID and RSSI for each network found
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+      Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*");
+      delay(10);
+    }
+  }
+  Serial.println("");
+  st = "<ol>";
+  for (int i = 0; i < n; ++i)
+  {
+    // Print SSID and RSSI for each network found
+    st += "<li>";
+    st += WiFi.SSID(i);
+    st += " (";
+    st += WiFi.RSSI(i);
+ 
+    st += ")";
+    st += (WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*";
+    st += "</li>";
+  }
+  st += "</ol>";
+  delay(100);
+  WiFi.softAP("how2electronics", "");
+  Serial.println("softap");
+  launchWeb();
+  Serial.println("over");
 }
